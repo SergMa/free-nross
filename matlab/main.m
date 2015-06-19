b084a48191fb5dbf60050c0cb39ae7de2763541c
Serg Mashkin <mashkh@yandex.ru>
2015-06-19 16:41:42 +0600

    MATLAB: new samples added, spectrograms added, s_diff output for measure quality

--- matlab/main.m
+++ matlab/main.m
@@ -10,58 +10,117 @@ clear all;
 close all;
 disp('started...');
 
-FS = 8000; % Sample (discretization) frequency, Hz
-TS = 1/FS; % Sample (discretization) period, sec
+FS = 8000;  % Sample (discretization) frequency, Hz
+TS = 1/FS;  % Sample (discretization) period, sec
 
-inputfilename = './input.wav'; % Name of file for input (noised) signal
-outfilename   = './out.wav';   % Name of file for output (clean) signal
+INPUT_FILENAME  = './input.wav'; % Name of file for input (noised) signal
+OUTPUT_FILENAME = './out.wav';   % Name of file for output (clean) signal
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Load input (noised) signal from wav-file
+% Load input (voice,noise) signals from wav-file
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% File name of wav-file with input (noised) signal
-%filename = './speech/lolita/lolita_yamz50.wav';
-%filename = './speech/lolita/lolita_diesel10.wav';
-%filename = './speech/lolita/lolita_diesel30.wav';
-%filename = './speech/lolita/lolita_diesel50.wav';
-filename = './speech/lolita/lolita_tractor50.wav';
+%voice_filename  = '../samples/cmu/sample1_8000.wav';
+%voice_filename = '../samples/cmu/sample2_8000.wav';
+voice_filename = '../samples/cmu/sample3_8000.wav';
+%voice_filename = '../samples/cmu/sample4_8000.wav';
+%voice_filename = '../samples/cmu/sample5_8000.wav';
+%voice_filename = '../samples/cmu/sample6_8000.wav';
+%voice_filename = '../samples/cmu/sample7_8000.wav';
 
-[x,ffs,bits] = wavread(filename);
-if ffs~=FS
-    error('invalid sample frequency of input wavefile');
+%noise_filename  = '../samples/noise/noise_white.wav';
+%noise_filename = '../samples/noise/noise_pink.wav';
+%noise_filename = '../samples/noise/noise_brown.wav';
+%noise_filename = '../samples/noise/noise_badbearing.wav';
+%noise_filename = '../samples/noise/noise_diesel.wav';
+noise_filename = '../samples/noise/noise_lacetti.wav';
+%noise_filename = '../samples/noise/noise_lacetti2.wav';
+%noise_filename = '../samples/noise/noise_tractor.wav';
+%noise_filename = '../samples/noise/noise_yamzdiesel.wav';
+
+[x_voice,ffs_voice,bits_voice] = wavread(voice_filename);
+if ffs_voice~=FS
+    error('invalid sample frequency of input voice wavefile');
 end
 
-% Make horizontal vector. If wavefile is stereo, use only first channel
+[x_noise,ffs_noise,bits_noise] = wavread(noise_filename);
+if ffs_noise~=FS
+    error('invalid sample frequency of input noise wavefile');
+end
+
+% Make horizontal vectors. If wavefiles are stereo, use only the first channels
 % now x has range [-1..+1]
-x = x.';
-x = x(1,:);
-N = size(x,2);
+x_voice = x_voice.';
+x_voice = x_voice(1,:);
+N_voice = size(x_voice,2);
+
+x_noise = x_noise.';
+x_noise = x_noise(1,:);
+N_noise = size(x_noise,2);
+
+% Limit lenght of signal, if needed
+TC = 10; %sec
+NC = TC*FS;
+N = min(N_voice,NC);
+x_voice = x_voice(1:N);
+
+% Add noise to voice
+VOICE_AMP_DB = -3; %dB
+NOISE_AMP_DB = -12; %dB
+x = mixer( x_voice, VOICE_AMP_DB, x_noise, NOISE_AMP_DB );
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Prepare signal to process
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+SPECTROGRAM_WIDTH = 512;
+SPECTROGRAM_OVR = 16;
 
 % Attenuate input signal (to prevent overflow)
 ATT = 1/16;  %scale down factor
 x = ATT * x;
-
-% Limit lenght of signal, if needed
-% TC = 15; %sec
-% NC = TC*FS;
-% N = min(N,NC);
-% x = x(1:N);
+N = length(x);
 
 % Convert sample numbers into time ticks (we will use this for plotting)
-t = 1:N;
-t = t/FS;
+t = (1:N)/FS;
 
 % Plot input signal
 figure(1);
+plot(t, 10^(VOICE_AMP_DB/20)*x_voice );
+title('input (voice) signal');
+xlabel('t,sec');
+ylabel('voice');
+ylim([-1, +1]);
+
+figure(2);
+plot(t,looper(10^(NOISE_AMP_DB/20)*x_noise,N));
+title('input (noise) signal');
+xlabel('t,sec');
+ylabel('noise');
+ylim([-1, +1]);
+
+figure(3);
 plot(t,x);
-title('input (noised) signal');
+title('input (voice+noise) signal');
 xlabel('t,sec');
 ylabel('x');
 ylim([-1*ATT, +1*ATT]);
 
+
+figure(4);
+s_voice = spectrogram( 10^(VOICE_AMP_DB/20)*x_voice, SPECTROGRAM_WIDTH, FS, SPECTROGRAM_OVR);
+title('input (voice) signal');
+
+figure(5);
+s_noise = spectrogram( looper(10^(VOICE_AMP_DB/20)*x_noise,N), SPECTROGRAM_WIDTH, FS, SPECTROGRAM_OVR);
+title('input (noise) signal');
+
+figure(6);
+s_signal = spectrogram( x, SPECTROGRAM_WIDTH, FS, SPECTROGRAM_OVR);
+title('input (voice+noise) signal');
+
 % Save input signal into input wavefile
-wavwrite(x',FS,bits,inputfilename);
+wavwrite( (x.')/ATT,FS,bits_voice,INPUT_FILENAME);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialize filters bank and energies measurers bank
@@ -103,7 +162,7 @@ vy_delay = my_delay_init( SUBBANDS, ENERGY_LEN );
 incrtime = zeros(1,SUBBANDS);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Îáðàáàòûâàåì ñèãíàë (îáðàáîòêà ðå÷è)
+% Main processing code
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 y = zeros(1,N);
@@ -193,7 +252,7 @@ for i=1:N
 
     % Slow envelope of signal
     TR = 0.8;    %Raise up factor: Ts/Tr, Tr=1ms, Ts=1/8000Hz=0.125ms
-    TF = 0.995;  %Fall down factor
+    TF = 0.993;  %Fall down factor
     for k=1:SUBBANDS
         if ey(k) > signal_e(k)
             signal_e(k) = ey(k) - (ey(k) - signal_e(k)) * TR;
@@ -319,81 +378,102 @@ for i=1:N
 end
 
 % Plot processed (clean) signal
-figure(2);
+figure(7);
 plot(t,y);
 title('processed (clean) signal');
 xlabel('t,sec');
 ylabel('y');
 
-% Plot help graphics
-figure(3);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_e1,'r',t,ttt_noise_e1,'b'); legend('e1','n1');
-subplot(6,1,3); plot(t,ttt_e2,'r',t,ttt_noise_e2,'b'); legend('e2','n2');
-subplot(6,1,4); plot(t,ttt_e3,'r',t,ttt_noise_e3,'b'); legend('e3','n3');
-subplot(6,1,5); plot(t,ttt_e4,'r',t,ttt_noise_e4,'b'); legend('e4','n4');
-subplot(6,1,6); plot(t,ttt_e5,'r',t,ttt_noise_e5,'b'); legend('e5','n5');
-
-figure(4);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_e6, 'r',t,ttt_noise_e6, 'b'); legend('e6','n6');
-subplot(6,1,3); plot(t,ttt_e7, 'r',t,ttt_noise_e7, 'b'); legend('e7','n7');
-subplot(6,1,4); plot(t,ttt_e8, 'r',t,ttt_noise_e8, 'b'); legend('e8','n8');
-subplot(6,1,5); plot(t,ttt_e9, 'r',t,ttt_noise_e9, 'b'); legend('e9','n9');
-subplot(6,1,6); plot(t,ttt_e10,'r',t,ttt_noise_e10,'b'); legend('e10','n10');
-
-figure(5);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_alpha_e1);  ylim([-0.1, 2.1]); legend('a1');
-subplot(6,1,3); plot(t,ttt_alpha_e2);  ylim([-0.1, 2.1]); legend('a2');
-subplot(6,1,4); plot(t,ttt_alpha_e3);  ylim([-0.1, 2.1]); legend('a3');
-subplot(6,1,5); plot(t,ttt_alpha_e4);  ylim([-0.1, 2.1]); legend('a4');
-subplot(6,1,6); plot(t,ttt_alpha_e5);  ylim([-0.1, 2.1]); legend('a5');
-
-figure(6);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_alpha_e6);  ylim([-0.1, 2.1]); legend('a6');
-subplot(6,1,3); plot(t,ttt_alpha_e7);  ylim([-0.1, 2.1]); legend('a7');
-subplot(6,1,4); plot(t,ttt_alpha_e8);  ylim([-0.1, 2.1]); legend('a8');
-subplot(6,1,5); plot(t,ttt_alpha_e9);  ylim([-0.1, 2.1]); legend('a9');
-subplot(6,1,6); plot(t,ttt_alpha_e10); ylim([-0.1, 2.1]); legend('a10');
-
-figure(7);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_signal_e1,'r',t,ttt_noise_e1,'b'); legend('s1','n1');
-subplot(6,1,3); plot(t,ttt_signal_e2,'r',t,ttt_noise_e2,'b'); legend('s2','n2');
-subplot(6,1,4); plot(t,ttt_signal_e3,'r',t,ttt_noise_e3,'b'); legend('s3','n3');
-subplot(6,1,5); plot(t,ttt_signal_e4,'r',t,ttt_noise_e4,'b'); legend('s4','n4');
-subplot(6,1,6); plot(t,ttt_signal_e5,'r',t,ttt_noise_e5,'b'); legend('s5','n5');
-
 figure(8);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_signal_e6, 'r',t,ttt_noise_e6, 'b'); legend('s6', 'n6');
-subplot(6,1,3); plot(t,ttt_signal_e7, 'r',t,ttt_noise_e7, 'b'); legend('s7', 'n7');
-subplot(6,1,4); plot(t,ttt_signal_e8, 'r',t,ttt_noise_e8, 'b'); legend('s8', 'n8');
-subplot(6,1,5); plot(t,ttt_signal_e9, 'r',t,ttt_noise_e9, 'b'); legend('s9', 'n9');
-subplot(6,1,6); plot(t,ttt_signal_e10,'r',t,ttt_noise_e10,'b'); legend('s10','n10');
+s_clean = spectrogram( y, SPECTROGRAM_WIDTH, FS, SPECTROGRAM_OVR);
+title('processed (voice+noise) signal');
 
+
+
+% now we have:
+% s_voice, s_noise, s_signal, s_clean
+s_diff = abs( s_clean - s_voice );
 figure(9);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_y1,'r'); legend('y1');
-subplot(6,1,3); plot(t,ttt_y2,'r'); legend('y2');
-subplot(6,1,4); plot(t,ttt_y3,'r'); legend('y3');
-subplot(6,1,5); plot(t,ttt_y4,'r'); legend('y4');
-subplot(6,1,6); plot(t,ttt_y5,'r'); legend('y5');
+imagesc(s_diff);
+axis xy;
 
-figure(10);
-subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
-subplot(6,1,2); plot(t,ttt_y6, 'r'); legend('y6');
-subplot(6,1,3); plot(t,ttt_y7, 'r'); legend('y7');
-subplot(6,1,4); plot(t,ttt_y8, 'r'); legend('y8');
-subplot(6,1,5); plot(t,ttt_y9, 'r'); legend('y9');
-subplot(6,1,6); plot(t,ttt_y10,'r'); legend('y10');
+avg_s_diff = mean(mean(s_diff));
+std_s_diff = std(std(s_diff));
+
+avg_s_diff
+std_s_diff
+
+% break;
+% 
+% % Plot help graphics
+% figure(3);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_e1,'r',t,ttt_noise_e1,'b'); legend('e1','n1');
+% subplot(6,1,3); plot(t,ttt_e2,'r',t,ttt_noise_e2,'b'); legend('e2','n2');
+% subplot(6,1,4); plot(t,ttt_e3,'r',t,ttt_noise_e3,'b'); legend('e3','n3');
+% subplot(6,1,5); plot(t,ttt_e4,'r',t,ttt_noise_e4,'b'); legend('e4','n4');
+% subplot(6,1,6); plot(t,ttt_e5,'r',t,ttt_noise_e5,'b'); legend('e5','n5');
+% 
+% figure(4);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_e6, 'r',t,ttt_noise_e6, 'b'); legend('e6','n6');
+% subplot(6,1,3); plot(t,ttt_e7, 'r',t,ttt_noise_e7, 'b'); legend('e7','n7');
+% subplot(6,1,4); plot(t,ttt_e8, 'r',t,ttt_noise_e8, 'b'); legend('e8','n8');
+% subplot(6,1,5); plot(t,ttt_e9, 'r',t,ttt_noise_e9, 'b'); legend('e9','n9');
+% subplot(6,1,6); plot(t,ttt_e10,'r',t,ttt_noise_e10,'b'); legend('e10','n10');
+% 
+% figure(5);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_alpha_e1);  ylim([-0.1, 2.1]); legend('a1');
+% subplot(6,1,3); plot(t,ttt_alpha_e2);  ylim([-0.1, 2.1]); legend('a2');
+% subplot(6,1,4); plot(t,ttt_alpha_e3);  ylim([-0.1, 2.1]); legend('a3');
+% subplot(6,1,5); plot(t,ttt_alpha_e4);  ylim([-0.1, 2.1]); legend('a4');
+% subplot(6,1,6); plot(t,ttt_alpha_e5);  ylim([-0.1, 2.1]); legend('a5');
+% 
+% figure(6);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_alpha_e6);  ylim([-0.1, 2.1]); legend('a6');
+% subplot(6,1,3); plot(t,ttt_alpha_e7);  ylim([-0.1, 2.1]); legend('a7');
+% subplot(6,1,4); plot(t,ttt_alpha_e8);  ylim([-0.1, 2.1]); legend('a8');
+% subplot(6,1,5); plot(t,ttt_alpha_e9);  ylim([-0.1, 2.1]); legend('a9');
+% subplot(6,1,6); plot(t,ttt_alpha_e10); ylim([-0.1, 2.1]); legend('a10');
+% 
+% figure(7);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_signal_e1,'r',t,ttt_noise_e1,'b'); legend('s1','n1');
+% subplot(6,1,3); plot(t,ttt_signal_e2,'r',t,ttt_noise_e2,'b'); legend('s2','n2');
+% subplot(6,1,4); plot(t,ttt_signal_e3,'r',t,ttt_noise_e3,'b'); legend('s3','n3');
+% subplot(6,1,5); plot(t,ttt_signal_e4,'r',t,ttt_noise_e4,'b'); legend('s4','n4');
+% subplot(6,1,6); plot(t,ttt_signal_e5,'r',t,ttt_noise_e5,'b'); legend('s5','n5');
+% 
+% figure(8);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_signal_e6, 'r',t,ttt_noise_e6, 'b'); legend('s6', 'n6');
+% subplot(6,1,3); plot(t,ttt_signal_e7, 'r',t,ttt_noise_e7, 'b'); legend('s7', 'n7');
+% subplot(6,1,4); plot(t,ttt_signal_e8, 'r',t,ttt_noise_e8, 'b'); legend('s8', 'n8');
+% subplot(6,1,5); plot(t,ttt_signal_e9, 'r',t,ttt_noise_e9, 'b'); legend('s9', 'n9');
+% subplot(6,1,6); plot(t,ttt_signal_e10,'r',t,ttt_noise_e10,'b'); legend('s10','n10');
+% 
+% figure(9);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_y1,'r'); legend('y1');
+% subplot(6,1,3); plot(t,ttt_y2,'r'); legend('y2');
+% subplot(6,1,4); plot(t,ttt_y3,'r'); legend('y3');
+% subplot(6,1,5); plot(t,ttt_y4,'r'); legend('y4');
+% subplot(6,1,6); plot(t,ttt_y5,'r'); legend('y5');
+% 
+% figure(10);
+% subplot(6,1,1); plot(t,x+1,'b',t,y-1,'r'); legend('x','y');
+% subplot(6,1,2); plot(t,ttt_y6, 'r'); legend('y6');
+% subplot(6,1,3); plot(t,ttt_y7, 'r'); legend('y7');
+% subplot(6,1,4); plot(t,ttt_y8, 'r'); legend('y8');
+% subplot(6,1,5); plot(t,ttt_y9, 'r'); legend('y9');
+% subplot(6,1,6); plot(t,ttt_y10,'r'); legend('y10');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Save processed (clean) signal into output wavefile
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-wavwrite(y',FS,bits,outfilename);
+wavwrite(y',FS,bits_voice,OUTPUT_FILENAME);
 
 disp('finished!');
