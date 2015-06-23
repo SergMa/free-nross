1d29e1fd9577e21861db4f14b60bb588c428bf0f
Serg Mashkin <mashkh@yandex.ru>
2015-06-23 10:17:24 +0600

    C: create test_fract.c/h file for fract16/32 tests

---
+++ c/test_fract.h
@@ -0,0 +1,15 @@
+/******************************************************************************/
+/* TEST PROGRAMM FOR FRACT16/FRACT32 UTILITES                                 */
+/* test_fract.h                                                               */
+/* (c) Sergei Mashkin, 2015                                                   */
+/******************************************************************************/
+
+#ifndef TEST_FRACT_H
+#define TEST_FRACT_H
+
+#include <my_fract.h>
+
+void test_fract16(void);
+void test_fract32(void);
+
+#endif /* TEST_FRACT_H */
